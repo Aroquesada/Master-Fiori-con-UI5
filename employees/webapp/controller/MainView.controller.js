1d97578d109aefce0372ce96f885ca908161e6b8
Aroquesada <aroquesada@hotmail.com>
2021-11-26 12:13:10 +0000

    First Commit

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -5,6 +5,8 @@ sap.ui.define([
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 * @param {typeof sap.ui.model.Filter} Filter
+	 * @param {typeof sap.ui.model.FilterOperator} FilterOperator     
 	 */
     function (Controller, Filter, FilterOperator) {
         "use strict";
@@ -65,6 +67,15 @@ sap.ui.define([
             var oModel = this.getView().getModel();
             oModel.setProperty("/EmployeeId", "");
             oModel.setProperty("/CountryKey", "");
+        };
+
+        function showPostalCode(oEvent) {
+            var itemPressed = oEvent.getSource();
+            var oContext = itemPressed.getBindingContext();
+            var objectContext = oContext.getObject();
+
+            sap.m.MessageToast.show(objectContext.PostalCode);
+
         }
 
         var Main = Controller.extend("Antonio.employees.controller.MainView", {});
@@ -87,6 +98,7 @@ sap.ui.define([
         Main.prototype.onInit = onInit;
         Main.prototype.onFilter = onFilter;
         Main.prototype.onClearFilter = onClearFilter;
+        Main.prototype.showPostalCode = showPostalCode;
 
         return Main;
     });
