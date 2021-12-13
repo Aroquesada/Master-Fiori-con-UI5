5113ffe3ff6b79ae0c87bafe43198547f0a0450a
Aroquesada <aroquesada@hotmail.com>
2021-12-13 15:49:11 +0000

    First Commit

---
+++ employees/webapp/controller/EmployeeDetails.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (controller) {
+
+    return controller.extend("Antonio.employees.controller.EmployeeDetails", {
+
+        onInit: function () {
+
+        },
+    });
+});
