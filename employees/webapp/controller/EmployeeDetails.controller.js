5e58dfc86bb6f09f6bc0400d7fd734b87122717f
Aroquesada <aroquesada@hotmail.com>
2021-12-20 11:35:10 +0000

    First Commit

--- employees/webapp/controller/EmployeeDetails.controller.js
+++ employees/webapp/controller/EmployeeDetails.controller.js
@@ -4,7 +4,7 @@ sap.ui.define([
 ], function (Controller, formatter) {
 
     function onInit() {
-
+        this._bus = sap.ui.getCore().getEventBus();
     };
 
     function onCreateIncidence() {
@@ -43,12 +43,48 @@ sap.ui.define([
 
     };
 
+    function onSaveIncidence(oEvent) {
+
+        var incidence = oEvent.getSource().getParent().getParent();
+        var incidenceRow = incidence.getBindingContext("incidenceModel");
+        this._bus.publish("incidence", "onSaveIncidence", { incidenceRow: incidenceRow.sPath.replace('/', '') });
+
+    };
+
+    function updateIncidenceCreationDate(oEvent) {
+
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+        contextObj.CreationDateX = true;
+
+    };
+
+    function updateIncidenceReason(oEvent) {
+
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+        contextObj.ReasonX = true;        
+
+    };
+
+    function updateIncidenceType(oEvent) {
+
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+        contextObj.TypeX = true;        
+
+    };
+
     var EmployeeDetails = Controller.extend("Antonio.employees.controller.EmployeeDetails", {});
 
     EmployeeDetails.prototype.onInit = onInit;
     EmployeeDetails.prototype.onCreateIncidence = onCreateIncidence;
     EmployeeDetails.prototype.onDeleteIncidence = onDeleteIncidence;
     EmployeeDetails.prototype.Formatter = formatter;
+    EmployeeDetails.prototype.onSaveIncidence = onSaveIncidence;
+    EmployeeDetails.prototype.updateIncidenceCreationDate = updateIncidenceCreationDate;
+    EmployeeDetails.prototype.updateIncidenceReason = updateIncidenceReason;
+    EmployeeDetails.prototype.updateIncidenceType = updateIncidenceType;
 
     return EmployeeDetails;
 
