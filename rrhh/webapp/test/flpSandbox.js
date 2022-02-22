2eec7f0525f51b3d1573b8e28a4201b445dd986f
Antonio Rodríguez Quesada <aroquesada@hotmail.com>
2022-02-22 09:28:53 +0000

    Proyecto Final

---
+++ rrhh/webapp/test/flpSandbox.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+	"sap/base/util/ObjectPath",
+	"sap/ushell/services/Container"
+], function (ObjectPath) {
+	"use strict";
+
+	// define ushell config
+	ObjectPath.set(["sap-ushell-config"], {
+		defaultRenderer: "fiori2",
+		bootstrapPlugins: {
+			"RuntimeAuthoringPlugin": {
+				component: "sap.ushell.plugins.rta",
+				config: {
+					validateAppVersion: false
+				}
+			},
+			"PersonalizePlugin": {
+				component: "sap.ushell.plugins.rta-personalize",
+				config: {
+					validateAppVersion: false
+				}
+			}
+		},
+		renderers: {
+			fiori2: {
+				componentData: {
+					config: {
+						enableSearch: false,
+						rootIntent: "Shell-home"
+					}
+				}
+			}
+		},
+		services: {
+			"LaunchPage": {
+				"adapter": {
+					"config": {
+						"groups": [{
+							"tiles": [{
+								"tileType": "sap.ushell.ui.tile.StaticTile",
+								"properties": {
+									"title": "RHH",
+									"targetURL": "#Antoniorrhh-display"
+								}
+							}]
+						}]
+					}
+				}
+			},
+			"ClientSideTargetResolution": {
+				"adapter": {
+					"config": {
+						"inbounds": {
+							"Antoniorrhh-display": {
+								"semanticObject": "Antoniorrhh",
+								"action": "display",
+								"description": "RRHH",
+								"title": "RHH",
+								"signature": {
+									"parameters": {}
+								},
+								"resolutionResult": {
+									"applicationType": "SAPUI5",
+									"additionalInformation": "SAPUI5.Component=Antonio.rrhh",
+									"url": sap.ui.require.toUrl("Antonio/rrhh")
+								}
+							}
+						}
+					}
+				}
+			},
+			NavTargetResolution: {
+				config: {
+					"enableClientSideTargetResolution": true
+				}
+			}
+		}
+	});
+
+	var oFlpSandbox = {
+		init: function () {
+			/**
+			 * Initializes the FLP sandbox
+			 * @returns {Promise} a promise that is resolved when the sandbox bootstrap has finshed
+			 */
+
+			// sandbox is a singleton, so we can start it only once
+			if (!this._oBootstrapFinished) {
+				this._oBootstrapFinished = sap.ushell.bootstrap("local");
+				this._oBootstrapFinished.then(function () {
+					sap.ushell.Container.createRenderer().placeAt("content");
+				});
+			}
+
+			return this._oBootstrapFinished;
+		}
+	};
+
+	return oFlpSandbox;
+});
---
+++ rrhh/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"Antonio/rrhh/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
